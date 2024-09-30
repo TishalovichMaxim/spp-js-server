b3d2bf3f2f7f5cc95bc4cf4abb2ffa4af6a878aa
Tishalovich Maxim <tishalovichm@gmail.com>
2024-09-30 14:03:48 +0300

    Remove old request handlers

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { TaskDao } from './dal/task-dao';
 import { Task, TaskStatus } from './model/task';
 import multer from 'multer';
 import cors from 'cors';
-import { ExtractionError, ReqTask, RespTask as RespTask, TaskExtracter, TaskMapper } from './dto/task-dto'
+import { ExtractionError, TaskExtracter, TaskMapper } from './dto/task-dto'
 import { TaskService } from './service/task-service'
 import { TaskValidator } from './service/validation/task-validator'
 import { ServiceError } from './service/exception/service-exception';
@@ -22,30 +22,6 @@ const taskValidator = new TaskValidator()
 const taskMapper = new TaskMapper()
 const taskService = new TaskService(taskDao, taskValidator, taskMapper)
 const taskExtracter = new TaskExtracter()
-
-//app.get('/tasks-page', (req, res) => {
-//    let tasks: Task[]
-//    let filter = req.query.filter
-//    let resFilter: undefined | number = undefined
-//
-//    console.log("Get: /tasks-page")
-//
-//    console.log(filter)
-//
-//    if (typeof filter == 'string' && Number.isInteger(Number.parseInt(filter))) {
-//        resFilter = Number.parseInt(filter)
-//    }
-//
-//    tasks = taskService.getAll(resFilter)
-//
-//    console.log("Tasks: %o", tasks)
-//    console.log("Filter = %o", resFilter)
-//
-//    res.render("tasks", {
-//        "tasks": tasks.map(t => new RespTask(t)),
-//        "filter": resFilter
-//    })
-//})
 
 app.get('/tasks', (req, res) => {
     res.status(200)
@@ -96,42 +72,6 @@ app.delete('/tasks/:id(\\d+)', (req, res) => {
     return res.status(204).send()
 })
 
-app.get('/add-task', (req, res) => {
-    res.render("add-task-form")
-})
-
-app.post('/add-task', (req, res) => {
-    console.log("POST: add task")
-
-    const reqTask = taskExtracter.extract(req.body)
-
-    console.log("Request task: %o", reqTask)
-
-    taskService.add(reqTask)
-
-    res.render("add-task-form")
-})
-
-app.post('/update-task/:id(\\d+)', (req, res) => {
-    const taskId = Number.parseInt(req.params.id)
-
-    console.log("POST: update task")
-
-    let updatedTask = taskExtracter.extract(req.body)
-    updatedTask.id = taskId
-
-    console.log("Task for update: %o", updatedTask)
-
-    let respTask = taskService.update(updatedTask)
-
-    const fileInfos = taskDao.getFiles(taskId)
-
-    console.log("Resp task: %o", respTask)
-    console.log("Attached files: %o", fileInfos)
-
-    res.render("update-task-form", { task: respTask, fileInfos: fileInfos })
-})
-
 app.post('/tasks/:id(\\d+)/files', upload.single('file'), (req, res) => {
     console.log("POST: file creation")
 
@@ -161,29 +101,6 @@ app.delete('/tasks/:taskId(\\d+)/files/:fileId', (req, res) => {
     res.status(204).send()
 })
 
-app.post('/tasks/:id(\\d+)/files/upload', upload.single('file'), (req, res) => {
-    if (!req.file) {
-        res.send("There is no file...")
-        return
-    }
-
-    const taskId = Number.parseInt(req.params.id)
-
-    console.log('POST: upload for task with id = %s', taskId)
-
-    taskService.addFile(taskId, req.file.filename, req.file.originalname)
-
-    res.redirect("/tasks/" + taskId)
-})
-
-app.post('/delete-task/:id(\\d+)', (req, res) => {
-    const taskId = Number.parseInt(req.params.id)
-
-    taskService.delete(taskId)
-
-    res.redirect('/tasks')
-})
-
 app.get('/tasks/:id(\\d+)/files/:file', (req, res) => {
     const taskId = Number.parseInt(req.params.id)
 
@@ -194,30 +111,6 @@ app.get('/tasks/:id(\\d+)/files/:file', (req, res) => {
     const fileInfo = taskService.getFileInfo(taskId, fileId)
 
     res.status(200).download(`./uploads/${fileId}`, fileInfo.name)
-})
-
-app.get('/tasks/download/:id(\\d+)/:file', (req, res) => {
-    const taskId = Number.parseInt(req.params.id)
-
-    const fileId = req.params.file
-
-    console.log("GET: download file: %s of task id = %s", fileId, taskId)
-
-    const fileInfo = taskService.getFileInfo(taskId, fileId)
-
-    res.status(200).download(`./uploads/${fileId}`, fileInfo.name)
-})
-
-app.post('/delete-file/:id(\\d+)/:file', (req, res) => {
-    const taskId = Number.parseInt(req.params.id)
-
-    const fileId = req.params.file
-
-    console.log("POST: delete file: %s of task id = %s", fileId, taskId)
-
-    taskService.deleteFileInfo(taskId, fileId)
-
-    res.redirect("/tasks/" + taskId)
 })
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
